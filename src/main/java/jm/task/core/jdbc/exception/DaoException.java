24d698cdcc6ba78ec4fd26a3182cb711c8e2c8f4
chuistov <108229048+chuistov@users.noreply.github.com>
2022-11-22 16:24:38 +0300

    JDBC and Hybernate template. JDBC CRUD ready.

--- src/main/java/jm/task/core/jdbc/exception/DaoException.java
+++ src/main/java/jm/task/core/jdbc/exception/DaoException.java
@@ -1,6 +1,10 @@
 package jm.task.core.jdbc.exception;
 
 public class DaoException extends RuntimeException {
+
+    public DaoException() {
+    }
+
     public DaoException(Throwable cause) {
         super(cause);
     }
